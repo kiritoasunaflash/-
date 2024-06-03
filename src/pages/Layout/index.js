b7ac3fa4c08a6ddae022dc76fd559803c4f0729a
kirito <1970201491@qq.com>
2024-06-03 18:43:14 +0800

    feat:月份

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,24 +1,62 @@
-import { getBillList } from "@/store/modules/billStore";
-import { Button } from "antd-mobile";
+import { TabBar } from "antd-mobile";
 import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { Outlet } from "react-router-dom";
+import { getBillList } from "@/store/modules/billStore";
+import "./index.scss";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
 
 const Layout = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBillList());
   }, [dispatch]);
+
+  // 切换菜单跳转路由
+  const navigate = useNavigate();
+  const swithRoute = (path) => {
+    if (path === "/month") {
+      navigate("/");
+    } else {
+      navigate(path);
+    }
+  };
   return (
-    <div>
-      <Outlet />
-      <div>
-        <Button color="primary" className="purple">
-          局部
-        </Button>
-        我是Layout
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={swithRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
       </div>
     </div>
   );
 };
+
 export default Layout;
