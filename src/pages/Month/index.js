b7b74ed36856b66e05b9d8fcb58111d2e42581d5
kirito <1970201491@qq.com>
2024-06-04 01:34:02 +0800

    feat:计算支付并使用useMemo进行缓存(类似computed)

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,9 +1,11 @@
 import { NavBar, DatePicker } from "antd-mobile";
 import "./index.scss";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import classNames from "classnames";
 import dayjs from "dayjs";
-import moment from "moment";
+import { useSelector } from "react-redux";
+import _ from "lodash";
+import { current } from "@reduxjs/toolkit";
 
 const Month = () => {
   const [dataVisble, setDataVisble] = useState(false);
@@ -11,11 +13,37 @@ const Month = () => {
     return new Date();
     // return new Date();
   });
+  const [monthList, setMonthList] = useState([]);
+  const { billList } = useSelector((state) => state.bill);
+  const groupList = useMemo(() => {
+    return _.groupBy(billList, (item) => dayjs(item.date).format("YYYY-MM"));
+  }, [billList]);
   const onConfirm = (value) => {
     setDateValue(value);
     setDataVisble(false);
-    console.log(dateValue);
+    const formatDate = dayjs(value).format("YYYY-MM");
+    setMonthList(groupList[formatDate]);
+    console.log(formatDate);
+    console.log(monthList);
   };
+  const monthResult = useMemo(() => {
+    console.log(monthList);
+    if (monthList && monthList?.length > 0) {
+      const payMoney = monthList
+        .filter((item) => item.type == "pay")
+        .reduce((prev, current) => prev + current.money, 0);
+      const income = monthList
+        .filter((item) => item.type == "income")
+        .reduce((prev, current) => prev + current.money, 0);
+      return {
+        payMoney,
+        income,
+        allTotal: payMoney + income,
+      };
+    } else {
+      return;
+    }
+  }, [monthList]);
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -41,15 +69,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.payMoney.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.allTotal.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
